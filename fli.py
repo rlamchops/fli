67a7529e0011110ee6f093f47348b5375b7bc08f
Raymond Lam <lamycraymond@hotmail.com>
2017-02-27 14:11:03 -0500

    complete, but no error checking

--- fli.py
+++ fli.py
@@ -1,8 +1,10 @@
 import getpass
 import os
+import shutil
 
 THRESHOLD = 400000
 EXTENSION = '.jpg'
+COUNTER = 0
 
 #first get the current user
 currentUser = getpass.getuser()
@@ -12,12 +14,24 @@ formatString = 'C:\\Users\\{}\\AppData\\Local\\Packages\\Microsoft.Windows.Conte
 fullPath = formatString.format(currentUser)
 
 #create a folder in the current directory to hold the images
-
+os.makedirs('fli', exist_ok = True)
+storagePath = os.path.realpath('fli')
 
 #change to the directory where the images are
 os.chdir(fullPath)
 
-#open files one at a time and perform the check
+#check files one at a time
 for name in os.listdir('.'):
+    #if one passes, copy it into the previously made directory
     if (os.stat(name).st_size > THRESHOLD):
-        print ("Found " name + EXTENSION + " : " + str(os.stat(name).st_size) + " bytes")
+        shutil.copy(name, storagePath)
+        print ("Found " + name + " : " + str(os.stat(name).st_size) + " bytes")
+
+os.chdir(storagePath)
+
+#the copied files have no extensions. need to add the specified one
+for name in os.listdir('.'):
+    os.replace(name, name + EXTENSION)
+    COUNTER += 1
+
+print ('Done. Made ' + str(COUNTER) + ' images. They can be found in ' + storagePath + '.')
